df9d9026e87f3cfb303d2b818c371411a934738d
Aditya <adityasikdar95@gmail.com>
2024-09-20 04:07:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,9 @@
     else {
         const accelerometerUpdate = e => {
             const dc = document.querySelectorAll('h3');
-            dc[0].innerText = e.accelerationIncludingGravity.x * 1;
-            dc[1].innerText = e.accelerationIncludingGravity.y * 1;
-            dc[2].innerText = e.accelerationIncludingGravity.z * 1;
+            dc[0].innerText = (e.accelerationIncludingGravity.x * 1).toFixed(3);
+            dc[1].innerText = (e.accelerationIncludingGravity.y * 1).toFixed(3);
+            dc[2].innerText = (e.accelerationIncludingGravity.z * 1).toFixed(3);
         }
         window.addEventListener("devicemotion", accelerometerUpdate, true);
     }
