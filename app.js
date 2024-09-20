1f264d3bb4686073e1c36bb96578a9f0689dbf89
Aditya <adityasikdar95@gmail.com>
2024-09-20 05:37:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 (() => {
-    let t = null;
+    // let t = null;
     if (!Boolean(window.DeviceMotionEvent)) {
         alert("No Accelerometer");
     }
     else {
         const dc = document.querySelectorAll('span');
         const accelerometerUpdate = e => {
-            if (t !== null) clearTimeout(t);
-            t = setTimeout(() => {
+            // if (t !== null) clearTimeout(t);
+            // t = setTimeout(() => {
+            setTimeout(() => {
                 dc[0].innerText = (e.accelerationIncludingGravity.x * 1).toFixed(2);
                 dc[1].innerText = (e.accelerationIncludingGravity.y * 1).toFixed(2);
                 dc[2].innerText = (e.accelerationIncludingGravity.z * 1).toFixed(2);
                 t = null;
-            }, 1000);
+            }, 5000);
         }
         window.addEventListener("devicemotion", accelerometerUpdate, true);
     }
