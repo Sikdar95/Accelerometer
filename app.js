fa434fd6dcb5a61c23ba7b4f72ea2e9eeaec567d
Aditya <adityasikdar95@gmail.com>
2024-09-20 13:33:41 +0530

    s

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 (() => {
     // let t = null;
+    const dc = document.querySelectorAll('span');
     if (!Boolean(window.DeviceMotionEvent)) {
         alert("No Accelerometer");
     }
     else {
-        const dc = document.querySelectorAll('span');
-        const accelerometerUpdate = e => {
-            // if (t !== null) clearTimeout(t);
-            // t = setTimeout(() => {
-            setTimeout(() => {
-                dc[0].innerText = (e.accelerationIncludingGravity.x * 1).toFixed(1);
-                dc[1].innerText = (e.accelerationIncludingGravity.y * 1).toFixed(1);
-                dc[2].innerText = (e.accelerationIncludingGravity.z * 1).toFixed(1);
-                t = null;
-            }, 2500);
-        }
-        window.addEventListener("devicemotion", accelerometerUpdate, true);
+        // const accelerometerUpdate = e => {
+        //     // if (t !== null) clearTimeout(t);
+        //     // t = setTimeout(() => {
+        //     setTimeout(() => {
+        //         dc[0].innerText = (e.accelerationIncludingGravity.x * 1).toFixed(1);
+        //         dc[1].innerText = (e.accelerationIncludingGravity.y * 1).toFixed(1);
+        //         dc[2].innerText = (e.accelerationIncludingGravity.z * 1).toFixed(1);
+        //         t = null;
+        //     }, 2500);
+        // }
+        // window.addEventListener("devicemotion", accelerometerUpdate, true);
 
-        const acl = new Accelerometer({ frequency: 10 });
+        const acl = new Accelerometer({ frequency: 2 });
         acl.addEventListener("reading", () => {
-            dc[3].innerText = acl.x;
-            dc[4].innerText = acl.y;
-            dc[5].innerText = acl.z;
+            dc[0].innerText = (+acl.x).toFixed(1);
+            dc[1].innerText = (+acl.y).toFixed(1);
+            dc[2].innerText = (+acl.z).toFixed(1);
         });
         acl.start();
     }
